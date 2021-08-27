a85e65fc3feef01899f2ba9b3d45b8b41fb15729
Ana Castrillo soria <acastril@c1r9s2.42madrid.com>
2021-08-27 13:33:19 +0200

    make calloc independant

--- ft_calloc.c
+++ ft_calloc.c
@@ -14,11 +14,16 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	void	*mem;
+	void			*mem;
+	size_t			i;
+	unsigned char	*temp;
 
 	mem = malloc(size * count);
 	if (!mem)
 		return (0);
-	ft_bzero(mem, size * count);
+	temp = mem;
+	i = 0;
+	while (i++ < size * count)
+		*temp++ = 0;
 	return (mem);
 }
